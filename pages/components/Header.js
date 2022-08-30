711e33cdeb473b193102d13c158362ed8af95cdf
Joao Mozelli Neto <joaomozelli@hotmail.com>
2022-08-29 22:23:46 -0300

    create Header and Home pages

---
+++ pages/components/Header.js
@@ -0,0 +1,14 @@
+export default function Header() {
+  return (
+    <nav className="navbar bg-dark">
+      <div className="container-fluid">
+        <a href="#" className="navbar-brand text-light">
+          Mozelli Marketing Digital
+        </a>
+        <a href="#" className="btn btn-sm btn-outline-warning">
+          Fale conosco
+        </a>
+      </div>
+    </nav>
+  );
+}
